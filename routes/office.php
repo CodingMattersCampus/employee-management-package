70433982eef2bd33c08ed44b6d5e531c50631b4e
gabbydgab <gamba@gabbydgab.com>
2018-04-28 16:03:19 +0800

    fixed routing middleware

--- routes/office.php
+++ routes/office.php
@@ -16,12 +16,12 @@ use Illuminate\Support\Facades\Route;
 $domain = \config('app.domain');
 Route::group(['domain' => "office.".$domain], function() {
 
-    Route::group(['middleware' => "auth:office"], function() {
+//    Route::group(['middleware' => "auth:office"], function() {
         Route::group(['prefix' => 'settings'], function(){
             Route::group(['prefix' => 'employees', "namespace" => "CodingMatters\EmployeeManagement\Http\Controllers"], function(){
         		Route::get('/', "ListingController@index")->name('office.employee.listing');
         		Route::post('/', "ListingController@create")->name('office.employee.create');
         	});
         });
-    });
+//    });
 });
