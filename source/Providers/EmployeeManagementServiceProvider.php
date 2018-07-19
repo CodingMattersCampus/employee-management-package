62bd705ca325e26a7331ae48a64ebb3d2aa62172
gabbydgab <gamba@gabbydgab.com>
2018-07-19 15:44:50 +0800

    added app domain config

--- source/Providers/EmployeeManagementServiceProvider.php
+++ source/Providers/EmployeeManagementServiceProvider.php
@@ -15,5 +15,11 @@ final class EmployeeManagementServiceProvider extends ServiceProvider
     public function boot() : void
     {
         $this->loadRoutesFrom(__DIR__ . '/../../routes/office.php');
+        
+        $configPath = $this->packagePath('config/domain.php');
+        $this->publishes([
+            $configPath => config_path('app-domain.php'),
+        ], 'config');
+        $this->mergeConfigFrom($configPath, 'app');
     }
 }
